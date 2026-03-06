f5f76b1a7fda061d3e02a7276295a5aa610fc6b7
moc <lev.ustinov@gmail.com>
2026-03-05 22:16:55 -0500

    scaffolding for arg parsing

--- src/cli.ts
+++ src/cli.ts
@@ -8,13 +8,36 @@ type Option = {
 
 } | null;
 
+const USAGE_MSG:string = "Usage: shrinkup <input> [output] [--quality <0-100>] [--overwrite] [--help]";
 
 function parseArgv():Option{
+
+    /**
+     * The way I see it for now
+     * shrinkup <input> [output] [--quality <0-100>] [--overwrite] [--help]
+     */
+
+    // extract the args 
+    const args:string[] = process.argv.slice(2); // ditch argv 0, 1...
+
+    if(args.length === 0) {
+        printUsage();
+        process.exit(1);
+    }
+
+    for(const arg of args){
+
+        
+
+    }
 
     console.log(process.argv);
     return null;
 };
 
+function printUsage():void{
+    console.log(USAGE_MSG);
+}
 
 
 
