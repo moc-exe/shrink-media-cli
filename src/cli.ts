a08c129b13a7b0660fb404c94d1842a9e1307c7c
moc <lev.ustinov@gmail.com>
2026-03-05 23:17:57 -0500

    first iteration of the cli parser

--- src/cli.ts
+++ src/cli.ts
@@ -4,11 +4,36 @@ type Option = {
 
     input: string, 
     output: string,
-    quality: number // [0-100] %
+    quality: number, // [0-100] %
+    overwrite:boolean
 
-} | null;
+};
 
+const ALLOWED_INPUT_EXTENSIONS= ["jpg","jpeg","png","webp"];
 const USAGE_MSG:string = "Usage: shrinkup <input> [output] [--quality <0-100>] [--overwrite] [--help]";
+type MessageMode = "log" | "error" | "warn";
+
+function printUsage():void{
+    promptUser("log", USAGE_MSG);
+};
+
+function promptUser(mode:MessageMode, msg:string):void{
+    
+    switch(mode){
+
+        case "log":
+            console.log(msg);
+            break;
+        case "error":
+            console.error(msg);
+            break;
+        case "warn":
+            console.warn(msg);
+            break;
+        default: console.log(msg);
+    }
+};
+
 
 function parseArgv():Option{
 
@@ -18,26 +43,80 @@ function parseArgv():Option{
      */
 
     // extract the args 
-    const args:string[] = process.argv.slice(2); // ditch argv 0, 1...
-
+    const args:string[] = process.argv.slice(2); // ditch argv idx=0, 1..., those are node runtime context
     if(args.length === 0) {
         printUsage();
         process.exit(1);
     }
 
-    for(const arg of args){
+    let parsedInput:string | undefined;
+    let parsedOutput:string | undefined;
+    let parsedQuality = 80;
+    let parsedOverwrite = false;
+    let inputExtension: string | undefined;
 
-        
+    for (let idx = 0; idx < args.length; idx++) {
 
+        const currentArg = args[idx];
+
+        if(currentArg === "--quality"){
+            const nextArg = args[idx + 1];
+
+            if (nextArg === undefined) {
+                promptUser("error", "Parsing error: missing value after --quality");
+                process.exit(1);
+            }
+            parsedQuality = Number(nextArg);
+            if(!Number.isInteger(parsedQuality) || parsedQuality < 0 || parsedQuality > 100){
+                promptUser("error", `Parsing error: quality must be [0-100]%, but provided=${parsedQuality}`);
+                printUsage();
+                process.exit(1);
+            }
+            continue;
+        }
+        else if(currentArg === "--overwrite"){
+            parsedOverwrite = true;
+        }
+        else if(currentArg?.startsWith("--")){
+            promptUser("error", `Error: Unknown option ${currentArg}`);
+        }
+        else if (parsedInput === undefined) {
+            parsedInput = currentArg;
+            inputExtension = currentArg?.split(".").at(1);
+
+        } else if (parsedOutput === undefined) {
+            parsedOutput = currentArg;
+        } else {
+            promptUser("error", `Error: unexpected argument ${currentArg}`);
+            process.exit(1);
+        }     
     }
 
-    console.log(process.argv);
-    return null;
-};
+    if (parsedInput === undefined) {
+            promptUser("error", "Parsing error: missing input");
+            printUsage();
+            process.exit(1);
+        }
+    const input: string = parsedInput;
+    
+    if(inputExtension === undefined || !ALLOWED_INPUT_EXTENSIONS.includes(inputExtension)){
+            promptUser("error", `Wrong extension, supported extensions are ${JSON.stringify(ALLOWED_INPUT_EXTENSIONS)}`);
+            process.exit(1);
+    }
 
-function printUsage():void{
-    console.log(USAGE_MSG);
-}
+    if (parsedOutput === undefined) {
+        parsedOutput = `compressed.${parsedInput}`;
+    }   
+    
+    return {
+
+            input: input, 
+            output : parsedOutput, 
+            quality : parsedQuality, 
+            overwrite: parsedOverwrite
+    }
+
+};
 
 
 
