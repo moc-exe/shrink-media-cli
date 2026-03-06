7b4f01c2e02984b7bda4fd82cb7cd7753930345e
moc <lev.ustinov@gmail.com>
2026-03-05 21:46:40 -0500

    some basic scaffolding and still configs

---
+++ src/cli.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+type Option = {
+
+    input: string, 
+    output: string,
+    quality: number // [0-100] %
+
+} | null;
+
+
+function parseArgv():Option{
+
+    console.log(process.argv);
+    return null;
+};
+
+
+
+
+async function main():Promise<void>{
+
+    
+};
